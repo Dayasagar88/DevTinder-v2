673266671957bc0db18aeff42f7cc4da7c995b56
Daya Sagar <152199702+Dayasagar88@users.noreply.github.com>
2026-05-04 11:14:06 +0530

    Dynamic baseURL for axios instance based on environment

--- client/src/axios/axios.js
+++ client/src/axios/axios.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const baseURL =
+  window.location.hostname === "localhost"
+    ? "http://localhost:5000/api"
+    : "https://devtinder-v2.onrender.com/api";
+
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL,
   withCredentials: true
 });
 
